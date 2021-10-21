5a4635cc26f01251640f2fc558293456df56d86b
felipe <baz@xds.com.br>
2021-10-21 14:42:18 -0300

    Adding Aumento Function

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Valor Aumento CSharp
+Valor-Aumento
+
+Para o aumento de salario, foi confeccionado uma função aumento(), onde ela recebe via stdin o salario de um certo funcionario, caso o mesmo esteja acima de R$ 1250, o salario do funcionario será aumentado em 10%.
+
+Assim inicialmente foi feito e recebimento do salario do funcionario como um float, visto que como se trata de um salario, é relevante o uso das casas decimais.
---
+++ aumento/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace aumento
+{
+    class Program
+    {
+        public static void Aumento(){
+            Console.WriteLine("Digite seu salario:\n");
+            double salario = double.Parse(Console.ReadLine());
+
+            if(salario >1250.00)
+            {
+                salario *= 1.10;
+            }else
+            {
+                salario *= 1.15;
+            }
+
+            string message = String.Format("O novo salario será de R$ {0,2:C}",salario);
+            Console.WriteLine(message);
+        }
+        static void Main(string[] args)
+        {
+            Aumento();
+        }
+    }
+}
